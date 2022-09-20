ac69593582296c50b887648b0fc4cafc0c82de49
LeoTheLegion <Halelt@live.com>
2022-09-20 13:38:53 -0400

    Created Core Folder

--- Crosshair.cs
+++ Crosshair.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using ShootingGallery.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
